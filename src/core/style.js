21b7d619c0ad11acc21c2b689a384dbe292f6247
khanhdaica <khanhdaica@kali.com.kali>
2022-11-02 04:51:29 -0400

    config deploy app

---
+++ src/core/style.js
@@ -0,0 +1,11 @@
+export const g_style = StyleSheet.create({
+    bg_E51937: {
+        backgroundColor: "#E51937"
+    },
+    text_0F1B2B: {
+        color: "#0F1B2B"
+    },
+    text_47CFFF: {
+        color: "#47CFFF"
+    }
+})
